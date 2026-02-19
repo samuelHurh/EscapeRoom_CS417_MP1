1cacfe182c115a4560716538a1b30362c1c6beb4
samuelHurh <66130364+samuelHurh@users.noreply.github.com>
2026-02-19 14:37:01 -0600

    finished

--- Assets/Scripts/GameStarter.cs
+++ Assets/Scripts/GameStarter.cs
@@ -22,6 +22,6 @@ public class GameStarter : MonoBehaviour
     
     public void RestartGame()
     {
-        SceneManager.LoadScene("StartScene");
+        SceneManager.LoadScene("StartScreen");
     }
 }
